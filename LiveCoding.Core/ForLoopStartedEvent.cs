1a8e3b81a562e068ab32e9b0d8d969d90a651449
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2013-08-14 20:10:30 +0400

    Added Quoter; added events for for loop and initial for loops rewriting

---
+++ LiveCoding.Core/ForLoopStartedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LiveCoding.Core
+{
+	public sealed class ForLoopStartedEvent : LiveEvent
+	{
+		public ForLoopStartedEvent()
+		{
+			LoopId = Guid.NewGuid();
+		}
+
+		public Guid LoopId { get; private set; }
+
+		public int LoopStartLineNumber { get; set; }
+	}
+}
---
+++ LiveCoding.Core/LiveEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace LiveCoding.Core
+{
+	public abstract class LiveEvent
+	{
+		protected LiveEvent()
+		{
+			TimestampUtc = DateTime.UtcNow;
+			ThreadId = Thread.CurrentThread.ManagedThreadId;
+		}
+
+		public int ThreadId { get; private set; }
+
+		public DateTime TimestampUtc { get; private set; }
+
+		public string MethodName { get; set; }
+	}
+}
