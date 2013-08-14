2141a407a8c4bd9899bdb85ecd43df55cc07d08f
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2013-08-14 17:35:14 +0400

    Switched from event to observable

--- LiveCoding.Core/ValueChange.cs
+++ LiveCoding.Core/ValueChange.cs
@@ -4,22 +4,25 @@ using System.Diagnostics;
 namespace LiveCoding.Core
 {
 	[DebuggerDisplay( "Change {VariableName} = {Value} @ {LineNumber} on {TimestampUtc}" )]
-	public sealed class ValueChange
+	public sealed class ValueChange : LiveEvent
 	{
 		public string VariableName { get; set; }
 
 		public object Value { get; set; }
 
-		public string MethodName { get; set; }
-
 		public int LineNumber { get; set; }
 
 		public string FilePath { get; set; }
 
-		public DateTime TimestampUtc { get; set; }
-
 		public int OriginalLineNumber { get; set; }
 		
 		public int ThreadId { get; set; }
+	}
+
+	public abstract class LiveEvent
+	{
+		public DateTime TimestampUtc { get; set; }
+
+		public string MethodName { get; set; }
 	}
 }
