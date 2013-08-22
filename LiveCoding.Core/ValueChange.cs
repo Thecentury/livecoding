b74ff496033213fa3a3b24498315e95f4b6ba25b
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2013-08-22 15:25:38 +0400

    For loop view works

--- LiveCoding.Core/ValueChange.cs
+++ LiveCoding.Core/ValueChange.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace LiveCoding.Core
 {
@@ -14,5 +15,10 @@ namespace LiveCoding.Core
 		public string FilePath { get; set; }
 
 		public int OriginalLineNumber { get; set; }
+
+		public override string ToString()
+		{
+			return String.Format( "{0}={1}", VariableName, Value );
+		}
 	}
 }
