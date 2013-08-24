ab866cfb9ef534e9ad86e4b50a2c8dc8fac28a01
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2013-08-24 22:13:35 +0400

    Correct class name extraction

--- LiveCoding.Extension/ClassFromNamespaceRewriter.cs
+++ LiveCoding.Extension/ClassFromNamespaceRewriter.cs
@@ -1,12 +1,19 @@
-﻿using Roslyn.Compilers.CSharp;
+﻿using System.Linq;
+using Roslyn.Compilers.CSharp;
 
 namespace LiveCoding.Extension
 {
-    public sealed class ClassFromNamespaceRewriter : SyntaxRewriter
-    {
-        public override SyntaxNode VisitNamespaceDeclaration( NamespaceDeclarationSyntax node )
-        {
-            return Visit( node.Members.First() );
-        }
-    }
+	public sealed class ClassFromNamespaceRewriter : SyntaxRewriter
+	{
+		public static readonly string LiveCodingWrapperClassName = "__LiveCodingWrapper";
+
+		public override SyntaxNode VisitNamespaceDeclaration( NamespaceDeclarationSyntax node )
+		{
+			return Syntax.ClassDeclaration( LiveCodingWrapperClassName )
+				.WithMembers(
+				Syntax.List(
+				node.Members.Select( m => Visit( m ) ).Cast<MemberDeclarationSyntax>()
+				) );
+		}
+	}
 }
