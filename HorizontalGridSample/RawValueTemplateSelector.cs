d7b91c4be28a90c94837379d69695489c8f5e2d9
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2013-08-30 18:41:00 +0400

    Templates for custom colors (at the beginning) rendering

---
+++ HorizontalGridSample/RawValueTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HorizontalGridSample
+{
+	public sealed class RawValueTemplateSelector : DataTemplateSelector
+	{
+		public override DataTemplate SelectTemplate( object item, DependencyObject container )
+		{
+			if ( item == null )
+			{
+				return null;
+			}
+
+			ContentPresenter presenter = (ContentPresenter)container;
+
+			FrameworkElement templatedParent = (FrameworkElement)presenter.TemplatedParent;
+
+			object foundResource = templatedParent.TryFindResource( new DataTemplateKey( item.GetType().Name ) );
+
+			if ( foundResource == null )
+			{
+				foundResource = templatedParent.TryFindResource( new DataTemplateKey( "system:Object" ) );
+			}
+
+			return foundResource as DataTemplate;
+		}
+	}
+}
