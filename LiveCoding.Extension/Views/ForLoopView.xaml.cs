df0aa68a4a164865b27741e7bf51a4802d1ef0df
mikhail.brinchuk <mikhail.brinchuk@localhost>
2013-09-04 22:52:41 +0400

    Transformed ForLoopView into UserControl and tuned its look

--- LiveCoding.Extension/Views/ForLoopView.xaml.cs
+++ LiveCoding.Extension/Views/ForLoopView.xaml.cs
@@ -1,46 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Dynamic;
 using System.Linq;
-using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 using LiveCoding.Core;
-using LiveCoding.Extension.Annotations;
-using LiveCoding.Extension.VisualStudio;
 using LiveCoding.Extension.VisualStudio.ForLoops;
 
 namespace LiveCoding.Extension.Views
 {
-	internal sealed class ForLoopView : Canvas
+	/// <summary>
+	/// Interaction logic for ForLoopView.xaml
+	/// </summary>
+	internal partial class ForLoopView : UserControl
 	{
-		private readonly ForLoopTag _data;
-
-		private readonly DataGrid _dataGrid = new DataGrid
-		{
-			CanUserResizeColumns = false,
-			CanUserResizeRows = false,
-			CanUserReorderColumns = false,
-			CanUserSortColumns = false,
-			AutoGenerateColumns = false,
-			IsReadOnly = true
-		};
-
 		public ForLoopView( ForLoopTag data )
 		{
-			_data = data;
+			InitializeComponent();
+			Canvas.SetTop( _dataGrid, -16 );
 
-			SetTop( _dataGrid, -data.LineHeight );
+			DataContext = data;
+		}
 
-			Children.Add( _dataGrid );
+		public DataGrid DataGrid
+		{
+			get { return _dataGrid; }
 		}
 
 		public void BeginWatching( ForLoopInfo loop )
@@ -89,46 +83,6 @@ namespace LiveCoding.Extension.Views
 			}
 
 			viewModel.AddChange( iteration.IterationNumber, valueChange );
-		}
-	}
-
-	internal sealed class ValueChangeViewModel : DynamicObject, INotifyPropertyChanged
-	{
-		private readonly Dictionary<int, ValueChange> _changes = new Dictionary<int, ValueChange>();
-
-		public void AddChange( int columnNumber, ValueChange change )
-		{
-			_changes.Add( columnNumber, change );
-
-			OnPropertyChanged( "p" + columnNumber );
-		}
-
-		private PropertyChangedEventHandler _propertyChangedEventHandler;
-		event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
-		{
-			add { _propertyChangedEventHandler += value; }
-			remove { _propertyChangedEventHandler = (PropertyChangedEventHandler)Delegate.Remove( _propertyChangedEventHandler, value ); }
-		}
-
-		public override bool TryGetMember( GetMemberBinder binder, out object result )
-		{
-			int columnNumber = Int32.Parse( binder.Name.Substring( 1 ) );
-			ValueChange change;
-			bool found = _changes.TryGetValue( columnNumber, out change );
-
-			result = change;
-
-			return found;
-		}
-
-		[NotifyPropertyChangedInvocator]
-		private void OnPropertyChanged( [CallerMemberName] string propertyName = null )
-		{
-			var handler = _propertyChangedEventHandler;
-			if ( handler != null )
-			{
-				handler( this, new PropertyChangedEventArgs( propertyName ) );
-			}
 		}
 	}
 }
---
+++ LiveCoding.Extension/Views/ValueChangeViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+using LiveCoding.Core;
+using LiveCoding.Extension.Annotations;
+
+namespace LiveCoding.Extension.Views
+{
+	internal sealed class ValueChangeViewModel : DynamicObject, INotifyPropertyChanged
+	{
+		private readonly Dictionary<int, ValueChange> _changes = new Dictionary<int, ValueChange>();
+
+		public void AddChange( int columnNumber, ValueChange change )
+		{
+			_changes.Add( columnNumber, change );
+
+			OnPropertyChanged( "p" + columnNumber );
+		}
+
+		private PropertyChangedEventHandler _propertyChangedEventHandler;
+		event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+		{
+			add { _propertyChangedEventHandler += value; }
+			remove { _propertyChangedEventHandler = (PropertyChangedEventHandler)Delegate.Remove( _propertyChangedEventHandler, value ); }
+		}
+
+		public override bool TryGetMember( GetMemberBinder binder, out object result )
+		{
+			int columnNumber = Int32.Parse( binder.Name.Substring( 1 ) );
+			ValueChange change;
+			if (_changes.TryGetValue(columnNumber, out change))
+			{
+				result = change;
+			}
+			else
+			{
+				result = null;
+			}
+
+			return true;
+		}
+
+		[NotifyPropertyChangedInvocator]
+		private void OnPropertyChanged( [CallerMemberName] string propertyName = null )
+		{
+			var handler = _propertyChangedEventHandler;
+			if ( handler != null )
+			{
+				handler( this, new PropertyChangedEventArgs( propertyName ) );
+			}
+		}
+	}
+}
