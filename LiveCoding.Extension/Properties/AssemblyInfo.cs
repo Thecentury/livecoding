66d122652aaf5b5973f759a2c54035cc7ec6876f
mikhail.brinchuk <mikhail.brinchuk@localhost>
2013-09-05 22:49:18 +0400

    Script compiler, that copies referenced assemblies to temp folder and then loads them in another appDomain

--- LiveCoding.Extension/Properties/AssemblyInfo.cs
+++ LiveCoding.Extension/Properties/AssemblyInfo.cs
@@ -1,25 +1,26 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LiveCoding.Exension")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LiveCoding.Exension")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle( "LiveCoding.Exension" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "LiveCoding.Exension" )]
+[assembly: AssemblyCopyright( "Copyright ©  2013" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4909bc3c-0151-40e1-88a4-1f666eb11cfc")]
+[assembly: Guid( "4909bc3c-0151-40e1-88a4-1f666eb11cfc" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
+
+[assembly: InternalsVisibleTo( "LiveCoding.Extension.Tests" )]
--- LiveCoding.Extension/ViewModels/ExecutingState.cs
+++ LiveCoding.Extension/ViewModels/ExecutingState.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using System.Security.Policy;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Threading;
@@ -17,45 +20,15 @@ using LiveCoding.Extension.VisualStudio.ForLoops;
 using LiveCoding.Extension.VisualStudio.VariableValues;
 using Roslyn.Compilers;
 using Roslyn.Compilers.CSharp;
-using Roslyn.Scripting;
-using Roslyn.Scripting.CSharp;
 using VSLangProj;
 
 namespace LiveCoding.Extension.ViewModels
 {
-	internal sealed class CodeCompiler : MarshalByRefObject
-	{
-		private readonly ScriptEngine _scriptEngine = new ScriptEngine();
-
-		public void SetupScriptEngine( List<string> namespaces, List<string> references )
-		{
-			Guid sessionId = Guid.NewGuid();
-
-			foreach ( string ns in namespaces )
-			{
-				_scriptEngine.ImportNamespace( ns );
-			}
-
-			string copyDirectory = Path.Combine( Path.GetTempPath(), sessionId.ToString() );
-			Directory.CreateDirectory( copyDirectory );
-
-			foreach ( string reference in references )
-			{
-				if ( File.Exists( reference ) )
-				{
-					
-				}
-			}
-		}
-	}
-
 	public sealed class ExecutingState : MethodExecutionStateBase
 	{
 		private sealed class MethodExecutionContext
 		{
 			public Stopwatch Stopwatch;
-
-			public Session Session;
 		}
 
 		private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
@@ -105,190 +78,215 @@ namespace LiveCoding.Extension.ViewModels
 				.Accept( new ClassFromNamespaceRewriter() )
 				.NormalizeWhitespace();
 
-			ScriptEngine engine = new ScriptEngine();
-
-			foreach ( var usingDirective in compilationUnit.Usings )
+			List<string> namespaces = new List<string>();
+			foreach ( var usingDirectiveSyntax in compilationUnit.Usings )
 			{
-				engine.ImportNamespace( usingDirective.Name.ToString() );
+				namespaces.Add( usingDirectiveSyntax.Name.ToString() );
 			}
-
 			foreach ( var namespaceDeclaration in compilationUnit.ChildNodes().OfType<NamespaceDeclarationSyntax>() )
 			{
-				engine.ImportNamespace( namespaceDeclaration.Name.ToString() );
+				namespaces.Add( namespaceDeclaration.Name.ToString() );
 			}
 
-			var project = ProjectHelper.GetContainingProject( filePath );
-
-			Configuration activeConfiguration = project.ConfigurationManager.ActiveConfiguration;
-			string outputName = project.Properties.Item( "OutputFileName" ).Value.ToString();
-			string outputPath = activeConfiguration.Properties.Item( "OutputPath" ).Value.ToString();
-			string projectPath = project.Properties.Item( "FullPath" ).Value.ToString();
-			string projectOutputFullPath = Path.Combine( projectPath, outputPath, outputName );
-
-			bool shouldBuild = project.IsDirty || !File.Exists( projectOutputFullPath );
-			if ( shouldBuild )
+			AppDomainSetup appDomainSetup = new AppDomainSetup
 			{
-				Solution solution = project.CodeModel.DTE.Solution;
+				ApplicationBase = Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location )
+			};
 
-				solution.SolutionBuild.BuildProject( solution.SolutionBuild.ActiveConfiguration.Name, project.UniqueName, true );
-				// todo brinchuk failed compilation handling
-			}
+			AppDomain domain = AppDomain.CreateDomain( "LiveCodingCompilation_" + Guid.NewGuid().ToString( "N" ), null, appDomainSetup, new PermissionSet( PermissionState.Unrestricted ), new StrongName[0] );
 
-			foreach ( Reference reference in project.GetReferences().References )
-			{
-				string path = reference.Path;
-				if ( File.Exists( path ) )
-				{
-					var assembly = Assembly.LoadFrom( path );
-					engine.AddReference( assembly );
-				}
-			}
-
-			if ( File.Exists( projectOutputFullPath ) )
-			{
-				engine.AddReference( Assembly.LoadFrom( projectOutputFullPath ) );
-			}
-			engine.AddReference( typeof( VariablesTracker ).Assembly );
-
-			//var compilation = Compilation.Create( "1.dll",
-			//	new CompilationOptions( OutputKind.DynamicallyLinkedLibrary, debugInformationKind: DebugInformationKind.Full ) )
-			//	.AddSyntaxTrees( rewritten.SyntaxTree )
-			//	.AddReferences(
-			//		project.GetReferences()
-			//			.References.OfType<Reference>()
-			//			.Select( r => MetadataReference.CreateAssemblyReference( r.Name ) ) )
-			//	.AddReferences( MetadataReference.CreateAssemblyReference( typeof( VariablesTracker ).Assembly.FullName ) );
-
-			var session = engine.CreateSession();
-
-			_context.Session = session;
-
+			CodeCompiler codeCompiler = null;
 			try
 			{
-				session.Execute( rewritten.ToString() );
-			}
-			catch ( CompilationErrorException exc )
-			{
-				throw;
-			}
+				codeCompiler = (CodeCompiler)domain.CreateInstanceAndUnwrap( Assembly.GetExecutingAssembly().FullName, typeof( CodeCompiler ).FullName );
 
-			VariablesTracker.ClearEvents();
+				List<string> references = new List<string>();
 
-			Dispatcher dispatcher = Owner.View.VisualElement.Dispatcher;
-			var view = Owner.View;
-			VariableValueTagger tagger = view.TextBuffer.Properties.GetProperty<VariableValueTagger>( typeof( VariableValueTagger ) );
+				var project = ProjectHelper.GetContainingProject( filePath );
+				foreach ( Reference reference in project.GetReferences().References )
+				{
+					string path = reference.Path;
+					references.Add( path );
+				}
+				Configuration activeConfiguration = project.ConfigurationManager.ActiveConfiguration;
+				string outputName = project.Properties.Item( "OutputFileName" ).Value.ToString();
+				string outputPath = activeConfiguration.Properties.Item( "OutputPath" ).Value.ToString();
+				string projectPath = project.Properties.Item( "FullPath" ).Value.ToString();
+				string projectOutputFullPath = Path.Combine( projectPath, outputPath, outputName );
 
-			var subscription = VariablesTracker.EventsObservable.OfType<ValueChange>().Subscribe( Observer.Create<ValueChange>(
-				change =>
+				bool shouldBuild = project.IsDirty || !File.Exists( projectOutputFullPath );
+				if ( shouldBuild )
+				{
+					Solution solution = project.CodeModel.DTE.Solution;
+
+					solution.SolutionBuild.BuildProject( solution.SolutionBuild.ActiveConfiguration.Name, project.UniqueName, true );
+					// todo brinchuk failed compilation handling
+				}
+
+				if ( File.Exists( projectOutputFullPath ) )
+				{
+					references.Add( projectOutputFullPath );
+				}
+				references.Add( typeof( VariablesTracker ).Assembly.Location );
+
+				codeCompiler.SetupScriptEngine( namespaces, references );
+
+				//var compilation = Compilation.Create( "1.dll",
+				//	new CompilationOptions( OutputKind.DynamicallyLinkedLibrary, debugInformationKind: DebugInformationKind.Full ) )
+				//	.AddSyntaxTrees( rewritten.SyntaxTree )
+				//	.AddReferences(
+				//		project.GetReferences()
+				//			.References.OfType<Reference>()
+				//			.Select( r => MetadataReference.CreateAssemblyReference( r.Name ) ) )
+				//	.AddReferences( MetadataReference.CreateAssemblyReference( typeof( VariablesTracker ).Assembly.FullName ) );
+
+				try
+				{
+					codeCompiler.Compile( rewritten.ToString() );
+				}
+				catch ( CompilationErrorException exc )
+				{
+					throw;
+				}
+
+				VariablesTracker.ClearEvents();
+
+				Dispatcher dispatcher = Owner.View.VisualElement.Dispatcher;
+				var view = Owner.View;
+				VariableValueTagger tagger =
+					view.TextBuffer.Properties.GetProperty<VariableValueTagger>( typeof( VariableValueTagger ) );
+
+				var subscription = VariablesTracker.EventsObservable.OfType<ValueChange>().Subscribe( Observer.Create<ValueChange>(
+					change =>
+					{
+						token.ThrowIfCancellationRequested();
+
+						dispatcher.BeginInvoke( () =>
+						{
+							var valueLine = view.TextSnapshot.GetLineFromLineNumber( change.OriginalLineNumber );
+							var span = view.GetTextElementSpan( valueLine.Start );
+
+							tagger.AddVariableChange( change, span );
+						}, DispatcherPriority.Normal );
+					} ) );
+
+				ForLoopTagger forLoopTagger = view.TextBuffer.Properties.GetProperty<ForLoopTagger>( typeof( ForLoopTagger ) );
+				var forLoopSubscription = VariablesTracker.ForLoops.Subscribe( loop =>
 				{
 					token.ThrowIfCancellationRequested();
 
 					dispatcher.BeginInvoke( () =>
 					{
-						var valueLine = view.TextSnapshot.GetLineFromLineNumber( change.OriginalLineNumber );
-						var span = view.GetTextElementSpan( valueLine.Start );
+						var loopLine = view.TextSnapshot.GetLineFromLineNumber( loop.LoopStartLineNumber );
+						var span = view.GetTextElementSpan( loopLine.End );
 
-						tagger.AddVariableChange( change, span );
+						forLoopTagger.BeginLoopWatch( loop, span );
 					}, DispatcherPriority.Normal );
-				} ) );
+				} );
 
-			ForLoopTagger forLoopTagger = view.TextBuffer.Properties.GetProperty<ForLoopTagger>( typeof( ForLoopTagger ) );
-			var forLoopSubscription = VariablesTracker.ForLoops.Subscribe( loop =>
-			{
-				token.ThrowIfCancellationRequested();
-
-				dispatcher.BeginInvoke( () =>
+				try
 				{
-					var loopLine = view.TextSnapshot.GetLineFromLineNumber( loop.LoopStartLineNumber );
-					var span = view.GetTextElementSpan( loopLine.End );
-
-					forLoopTagger.BeginLoopWatch( loop, span );
-				}, DispatcherPriority.Normal );
-			} );
-
-			try
-			{
-				if ( Owner.Data.Call == null )
-				{
-					var snapshot = Owner.Data.SnapshotSpan.Snapshot;
-
-					int methodStartPosition = Owner.Data.SnapshotSpan.Start.Position;
-					var line = snapshot.GetLineFromLineNumber( snapshot.GetLineNumberFromPosition( methodStartPosition ) );
-					string text = line.GetText();
-
-					var methodTree = SyntaxTree.ParseText( text, cancellationToken: token );
-
-					var methodDeclaration = methodTree.GetRoot( token ).ChildNodes().OfType<MethodDeclarationSyntax>().First();
-
-					string methodName = methodDeclaration.Identifier.ValueText;
-
-					var liveCodingClass = rewritten.ChildNodes().OfType<ClassDeclarationSyntax>().First();
-
-					var classDeclaration = liveCodingClass.ChildNodes().OfType<ClassDeclarationSyntax>().Where( cd => cd.Span.Contains( methodStartPosition ) ).First();
-
-					string className = classDeclaration.Identifier.ValueText;
-					string fullClassName = ClassFromNamespaceRewriter.LiveCodingWrapperClassName + "." + className;
-
-					string parameterValues = methodDeclaration.ParameterList.GetDefaultParametersValuesString();
-
-					_context.Stopwatch = Stopwatch.StartNew();
-
-					if ( methodDeclaration.IsStatic() )
+					if ( Owner.Data.Call == null )
 					{
-						bool isStaticCtor = methodName == className;
-						if ( isStaticCtor )
+						var snapshot = Owner.Data.SnapshotSpan.Snapshot;
+
+						int methodStartPosition = Owner.Data.SnapshotSpan.Start.Position;
+						var line = snapshot.GetLineFromLineNumber( snapshot.GetLineNumberFromPosition( methodStartPosition ) );
+						string text = line.GetText();
+
+						var methodTree = SyntaxTree.ParseText( text, cancellationToken: token );
+
+						var methodDeclaration = methodTree.GetRoot( token ).ChildNodes().OfType<MethodDeclarationSyntax>().First();
+
+						string methodName = methodDeclaration.Identifier.ValueText;
+
+						var liveCodingClass = rewritten.ChildNodes().OfType<ClassDeclarationSyntax>().First();
+
+						var classDeclaration =
+							liveCodingClass.ChildNodes()
+								.OfType<ClassDeclarationSyntax>()
+								.Where( cd => cd.Span.Contains( methodStartPosition ) )
+								.First();
+
+						string className = classDeclaration.Identifier.ValueText;
+						string fullClassName = ClassFromNamespaceRewriter.LiveCodingWrapperClassName + "." + className;
+
+						string parameterValues = methodDeclaration.ParameterList.GetDefaultParametersValuesString();
+
+						_context.Stopwatch = Stopwatch.StartNew();
+
+						if ( methodDeclaration.IsStatic() )
 						{
-							throw new CannotExecuteException( "Cannot execute static ctor" );
-						}
+							bool isStaticCtor = methodName == className;
+							if ( isStaticCtor )
+							{
+								throw new CannotExecuteException( "Cannot execute static ctor" );
+							}
 
-						session.Execute( String.Format( "{0}.{1}( {2} );", fullClassName, methodName, parameterValues ) );
-					}
-					else
-					{
-						string instanceVariableName = "__liveCodingInstance_" + Guid.NewGuid().ToString( "N" );
-
-						bool hasParameterlessConstructor = classDeclaration.ChildNodes()
-							.OfType<ConstructorDeclarationSyntax>()
-							.Where( c => c.ParameterList.Parameters.Count == 0 )
-							.Any();
-
-						bool doesnotHaveConstructorDeclarationAtAll =
-							!classDeclaration.ChildNodes().OfType<ConstructorDeclarationSyntax>().Any();
-
-						if ( hasParameterlessConstructor || doesnotHaveConstructorDeclarationAtAll )
-						{
-							session.Execute( String.Format( "var {0} = new {1}();", instanceVariableName, fullClassName ) );
+							codeCompiler.Compile( String.Format( "{0}.{1}( {2} );", fullClassName, methodName, parameterValues ) );
 						}
 						else
 						{
-							var firstConstructor = classDeclaration.ChildNodes().OfType<ConstructorDeclarationSyntax>().First();
+							string instanceVariableName = "__liveCodingInstance_" + Guid.NewGuid().ToString( "N" );
 
-							string constructorParameters = firstConstructor.ParameterList.GetDefaultParametersValuesString();
-							session.Execute(
-								String.Format( "var {0} = new {1}( {2} );", instanceVariableName, fullClassName, constructorParameters ) );
-						}
+							bool hasParameterlessConstructor = classDeclaration.ChildNodes()
+								.OfType<ConstructorDeclarationSyntax>()
+								.Where( c => c.ParameterList.Parameters.Count == 0 )
+								.Any();
 
-						bool ctorInvocation = className == methodName;
-						if ( !ctorInvocation )
-						{
-							session.Execute( String.Format( "{0}.{1}( {2} )", instanceVariableName, methodName, parameterValues ) );
+							bool doesnotHaveConstructorDeclarationAtAll =
+								!classDeclaration.ChildNodes().OfType<ConstructorDeclarationSyntax>().Any();
+
+							if ( hasParameterlessConstructor || doesnotHaveConstructorDeclarationAtAll )
+							{
+								codeCompiler.Compile( String.Format( "var {0} = new {1}();", instanceVariableName, fullClassName ) );
+							}
+							else
+							{
+								var firstConstructor = classDeclaration.ChildNodes().OfType<ConstructorDeclarationSyntax>().First();
+
+								string constructorParameters = firstConstructor.ParameterList.GetDefaultParametersValuesString();
+								codeCompiler.Compile(
+									String.Format( "var {0} = new {1}( {2} );", instanceVariableName, fullClassName, constructorParameters ) );
+							}
+
+							bool ctorInvocation = className == methodName;
+							if ( !ctorInvocation )
+							{
+								codeCompiler.Compile( String.Format( "{0}.{1}( {2} )", instanceVariableName, methodName, parameterValues ) );
+							}
 						}
 					}
+					else
+					{
+						_context.Stopwatch = Stopwatch.StartNew();
+						codeCompiler.Compile( Owner.Data.Call );
+					}
 				}
-				else
+				finally
 				{
-					_context.Stopwatch = Stopwatch.StartNew();
-					session.Execute( Owner.Data.Call );
+					if ( _context.Stopwatch != null )
+					{
+						_context.Stopwatch.Stop();
+					}
+					subscription.Dispose();
+					forLoopSubscription.Dispose();
 				}
 			}
 			finally
 			{
-				if ( _context.Stopwatch != null )
+				string tempDirectory = null;
+				if ( codeCompiler != null )
 				{
-					_context.Stopwatch.Stop();
+					tempDirectory = codeCompiler.TempDirectory;
 				}
-				subscription.Dispose();
-				forLoopSubscription.Dispose();
+
+				AppDomain.Unload( domain );
+
+				if ( tempDirectory != null )
+				{
+					Directory.Delete( tempDirectory, true );
+				}
 			}
 		}
 
