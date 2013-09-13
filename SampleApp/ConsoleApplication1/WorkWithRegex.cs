0e27c6a74a48d998c92ff60578630eb4b060cd56
Mikhail Brinchuk <Mikhail Brinchuk@localhost>
2013-09-13 12:23:09 +0400

    Loop lists caching, loops adorners updating

--- SampleApp/ConsoleApplication1/WorkWithRegex.cs
+++ SampleApp/ConsoleApplication1/WorkWithRegex.cs
@@ -7,62 +7,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Loops
-    {
-        public void ForLoopWithOneLineBody()
-        {
-            int j = 0;
-            for (int i = 0; i < 10; i++)
-                j += i;
-        }
-    }
-
-    class BinarySearch
-    {
-        public void PerformBinarySearch()
-        {
-            const int count = 100;
-
-            int[] array = new int[count];
-            for (int i = 0; i < count; i++)
-            {
-                array[i] = i;
-                string s = i.ToString();
-            }
-
-            int needle = count / 2 - 1;
-            int min = 0;
-            int max = count;
-
-            while (min <= max)
-            {
-                int mid = (max + min) / 2;
-                int value = array[mid];
-                if (value == needle)
-                {
-                    break;
-                }
-                else if (value < needle)
-                {
-                    min = mid + 1;
-                }
-                else
-                {
-                    max = mid - 1;
-                }
-            }
-        }
-
-        public void SomeLoop()
-        {
-            for (int i = 0; i < 1; i++)
-            {
-                Regex r = new Regex(@"\d");
-            }
-        }
-    }
-
-    class WorkWithRegex
+	class WorkWithRegex
     {
         public void Method()
         {
