1d13f5f457fc787c0834b97bbea77c8981ea0b5d
mikhail.brinchuk <mikhail.brinchuk@localhost>
2013-09-15 22:55:41 +0400

    Fixed loop iterator value display for foreach loops; several loops samples

--- SampleApp/ConsoleApplication1/Loops.cs
+++ SampleApp/ConsoleApplication1/Loops.cs
@@ -1,12 +1,41 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+using System.Linq;
+namespace ConsoleApplication1
 {
-	class Loops
-	{
-		public void ForLoopWithOneLineBody()
-		{
-			int j = 0;
-			for (int i = 0; i < 10; i++)
-				j += i;
-		}
-	}
+    class Loops
+    {
+        public void ForLoopWithOneLineBody()
+        {
+            int j = 1;
+            for (int i = 1; i < 10; i++)
+                j += i;
+        }
+
+        public void ForeachLoop()
+        {
+            foreach (var item in Enumerable.Range(3, 5))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void While()
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                i++;
+                Console.WriteLine(i + "!");
+            }
+        }
+
+        public void DoWhile()
+        {
+            int i = 0;
+            do
+            {
+                i++;
+            } while (i < 10);
+        }
+    }
 }
