353c71a69bbb26568736f4380afab56c4b8d8ca5
Mikhail Brinchuk <Mikhail Brinchuk@localhost>
2013-09-18 19:11:10 +0400

    Started to work on invocations as a part of other statements

--- SampleApp/ConsoleApplication1/Methods.cs
+++ SampleApp/ConsoleApplication1/Methods.cs
@@ -11,6 +11,30 @@ namespace ConsoleApplication1
         public void M()
         {
             Console.WriteLine(new Random().NextDouble());
+
+            Console.WriteLine("{0} {1}", Proxy.Pass(1 + 1), Proxy.Pass(true == false));
+
+            int i = Proxy.Pass(1 + 2); Proxy.Pass(1 + 1);
+            Proxy.Pass(2 + "3");
+
+            ClassWithProperty<int>.P = Proxy.Pass(1 + 2);
+        }
+    }
+
+    static class ClassWithProperty<T>
+    {
+        public static T P
+        {
+            get { return default(T); }
+            set { }
+        }
+    }
+
+    static class Proxy
+    {
+        public static T Pass<T>(T value)
+        {
+            return value;
         }
     }
 }
