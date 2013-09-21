24f40c9c9e8ed373241d721489403064a5c1270c
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2013-09-21 19:27:56 +0400

    Added nlog bootstrapper

---
+++ LiveCoding.Extension/NlogBootstrapper.cs
@@ -0,0 +1,48 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
+
+namespace LiveCoding.Extension
+{
+	public static class NlogBootstrapper
+	{
+		private static volatile bool _initialized;
+		private static readonly object _lock = new object();
+
+		public static void Initialize()
+		{
+			if ( _initialized )
+			{
+				return;
+			}
+			lock ( _lock )
+			{
+				if ( _initialized )
+				{
+					return;
+				}
+
+				var layout = new SimpleLayout( "${level} [${machinename}] [${pad:padding=3:${threadid}}] ${longdate}	${logger:shortName=True} ${message} ${exception:format=ToString}" );
+				Target t = new LogentriesTarget
+				{
+					Token = "18bd643b-3620-41b3-aff1-4a4d40079e59",
+					Debug = true,
+					HttpPut = false,
+					Ssl = false,
+					Name = "logentries",
+					Layout = layout,
+				};
+
+				var configuration = new LoggingConfiguration();
+
+				configuration.AddTarget( "logentries", t );
+				configuration.LoggingRules.Add( new LoggingRule( "*", LogLevel.Trace, t ) );
+
+				LogManager.Configuration = configuration;
+
+				_initialized = true;
+			}
+		}
+	}
+}
