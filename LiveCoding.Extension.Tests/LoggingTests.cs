55518d02de06025f24f4d29f30a7c8c717585794
mikhail.brinchuk <mikhail.brinchuk@localhost>
2013-09-21 18:53:48 +0400

    Added NLog and LogEntries NLog target in order to find the reason for NLOg logging doesn't work

--- LiveCoding.Extension.Tests/LoggingTests.cs
+++ LiveCoding.Extension.Tests/LoggingTests.cs
@@ -1,4 +1,7 @@
-﻿using NLog;
+﻿using System;
+using System.Threading;
+using NLog;
+using NLog.Common;
 using NLog.Config;
 using NLog.Layouts;
 using NLog.Targets;
@@ -12,23 +15,39 @@ namespace LiveCoding.Extension.Tests
 		[Test]
 		public void WriteToLog()
 		{
+			var layout = new SimpleLayout( "${date:format=ddd MMM dd} ${time:format=HH:mm:ss} ${date:format=zzz yyyy} [${machinename}] [${threadid}] ${logger} ${LEVEL} ${message} ${exception:format=tostring}" );
 			Target t = new LogentriesTarget
 			{
 				Token = "18bd643b-3620-41b3-aff1-4a4d40079e59",
 				Debug = true,
-				HttpPut = true,
+				HttpPut = false,
 				Ssl = false,
 				Name = "logentries",
-				Layout = new SimpleLayout( "${date:format=ddd MMM dd} ${time:format=HH:mm:ss} ${date:format=zzz yyyy} [${machinename}] [${threadid}] ${logger} ${LEVEL} ${message} ${exception:format=tostring}" )
+				Layout = layout,
 			};
 
-			LogManager.Configuration = new LoggingConfiguration();
+			var configuration = new LoggingConfiguration();
 
-			LogManager.Configuration.AddTarget( "logentries", t );
-			LogManager.Configuration.LoggingRules.Add( new LoggingRule( "*", LogLevel.Trace, t ) ); 
-			
+			configuration.AddTarget( "logentries", t );
+			configuration.LoggingRules.Add( new LoggingRule( "*", LogLevel.Trace, t ) );
+
+			LogManager.Configuration = configuration;
+
 			Logger logger = LogManager.GetCurrentClassLogger();
-			logger.Info( "123" );
+
+			t.WriteAsyncLogEvent( new AsyncLogEventInfo( new LogEventInfo( LogLevel.Info, logger.Name, "123" ), e =>
+			{
+				Console.WriteLine( e );
+			} ) );
+
+			for ( int i = 0; i < 10; i++ )
+			{
+				logger.Info( "123" );
+			}
+
+			LogManager.Flush();
+
+			Thread.Sleep( 2000 );
 		}
 	}
 }
